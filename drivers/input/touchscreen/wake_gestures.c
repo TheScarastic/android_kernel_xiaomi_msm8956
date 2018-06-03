b31c866e9fd25bff0a53cd4a22bf13823ad78dd1
TheScarastic <warabhishek@gmail.com>
2018-06-03 22:26:24 +0530

    wake_gesture: enanle all
     * coz akhil asked

--- drivers/input/touchscreen/wake_gestures.c
+++ drivers/input/touchscreen/wake_gestures.c
@@ -39,9 +39,9 @@
 /* Tuneables */
 #define WG_DEBUG		0
 #define WG_DEFAULT		0
-#define DT2W_DEFAULT		0
-#define S2W_DEFAULT		0
-#define S2S_DEFAULT		0
+#define DT2W_DEFAULT		1
+#define S2W_DEFAULT		1
+#define S2S_DEFAULT		1
 #define WG_PWRKEY_DUR           60
 
 /* Redmi Note 3 */
